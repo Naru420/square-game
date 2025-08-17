bc422882e1a5cce0a159c3a6c5797e2922528120
Naru420 <120468292+Naru420@users.noreply.github.com>
2025-08-17 14:58:40 +0200

    fuck

--- square/turn_based_battel.gd
+++ square/turn_based_battel.gd
@@ -3,18 +3,26 @@ extends Node2D
 
 @export var Party : Array[Charakter] 
 @export var Enemy_Party : Array[Charakter] 
+var all_participants : Array[Charakter] = Party + Enemy_Party
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	CreateTurnOrder()
+	getCurrentTurn()
+	#CreateTurnOrder(5)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+#func _process(delta: float) -> void:
+#	pass
+	
+#func CreateTurnOrder(int turns):
+	#for 
+	#pass
+
+func getCurrentTurn():
+	
+	for i in all_participants.size():
+		print(i) 
+		pass
 	pass
-	
-func CreateTurnOrder():
-	
-	pass
-	
